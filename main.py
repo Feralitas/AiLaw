b3b52ba2cce47287fafed5d2ecafa8f1b7a327f2
tomixed <marius@invisibletower.de>
2020-09-19 21:21:11 +0200

    even better commit.... the best!

---
+++ main.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.clock import Clock
+from kivy.graphics import Color, Rectangle, Line
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.image import AsyncImage
+from kivy.logger import Logger
+import datetime
+import win32gui
+import win32con
+import win32api
+import kivy.utils
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.uix.filechooser import FileChooserListView, FileChooserIconView
+from kivy.uix.image import Image
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.gridlayout import GridLayout
+import random
+import webview
+from kivy.config import Config
+import win32com.client
+
+Config.set('graphics', 'borderless', 'true')
+Config.set('graphics', 'position', 'custom')
+Config.set('graphics', 'top', '300')
+Config.set('graphics', 'left', '300')
+
+
+
+import sys
+import fileinput
+import queue
+from mouseInterfaces import start_mouse_event_listener_thread, stop_mouse_event_listener_thread, command_queue
+from markedtext import get_selected_text
+
+class AFA(App):
+    globalText = "test text"
+    outTxt = Label(text='Testsss', markup=True)
+    def callbackWriteText(self):
+        try:
+            cmd = command_queue.get_nowait()
+            if cmd == "Gesture button":
+                
+                self.globalText = get_selected_text() #MW test
+                #webview.create_window('AiLaw', self.globalText)
+                #webview.start()
+                if len(self.globalText) > 0:
+                    self.outTxt.text = '[size=16][color=FFFFFF][font=RobotoMono-Regular]'+ self.globalText +'[/font][/color][/size]'
+                self.startUp()
+                
+                self.StatusOfApp = 1
+            if cmd == "Program end":
+                self.stop()
+        except queue.Empty:
+            pass
+
+    def getHandleOfThisWindow(self):
+        if self.handle == 0:
+            self.handle = win32gui.FindWindow(None, "Advocatum facile adiutor")
+        return self.handle
+
+    def makeItTransparent(self, alpha):# Set alpha between 0 and 1. 0 no opacity, 1 invisible
+        alpha = int((1-alpha) * 255)
+        win32gui.SetWindowLong(self.getHandleOfThisWindow(), win32con.GWL_EXSTYLE, win32gui.GetWindowLong(self.getHandleOfThisWindow(), win32con.GWL_EXSTYLE) | win32con.WS_EX_LAYERED)   # Make it a layered window
+        win32gui.SetLayeredWindowAttributes(self.getHandleOfThisWindow(), win32api.RGB(0, 0, 0), alpha, win32con.LWA_ALPHA)        # make it transparent (alpha between 0 and 255)
+    
+    def makeItForeground(self):
+        shell = win32com.client.Dispatch("WScript.Shell")
+        shell.SendKeys('%')
+        win32gui.SetForegroundWindow(self.getHandleOfThisWindow())
+
+    def PositionToMouse(self, width, height):
+        shell = win32com.client.Dispatch("WScript.Shell")
+        shell.SendKeys('%')
+        flags, hcursor, (x,y) = win32gui.GetCursorInfo()
+        win32gui.SetWindowPos(self.getHandleOfThisWindow(), win32con.HWND_TOP, round(x - width/2), y - height - 80, width, height, win32con.SWP_SHOWWINDOW)
+        self.makeItForeground()
+
+    def hibernate(self):
+        win32gui.ShowWindow(self.getHandleOfThisWindow(), 0)
+        self.StatusOfApp = 0
+
+    def startUp(self):
+        win32gui.ShowWindow(self.getHandleOfThisWindow(),1)
+        self.PositionToMouse(600, 400)
+        self.makeItTransparent(.2)
+        self.makeItForeground()
+        self.makeItForeground()
+
+    def on_stop(self):
+        stop_mouse_event_listener_thread()
+
+    def build(self):
+        ##Experiment
+        self.handle = 0  #init
+        self.StatusOfApp = 0
+        self.preller = 0
+        layout = GridLayout(cols = 1)
+
+        ##Exp End
+        self.title = 'Advocatum facile adiutor'
+        
+
+        
+        layout.add_widget(self.outTxt)
+        btn = Button(text='Hello World')
+        layout.add_widget(btn)
+        txt=Label(text='label text')
+        layout.add_widget(txt)
+
+
+        Clock.schedule_interval(lambda dt: self.callbackWriteText(), 0.01)
+    #    Clock.schedule_once(lambda dt: self.hibernate(), 0.2) # initialiying the hibernate after start up
+        return layout
+
+
+start_mouse_event_listener_thread()
+
+try:
+    AFA().run()
+except KeyboardInterrupt:
+    Logger.info("main: Ctrl+C detected. Terminate!")
+    stop_mouse_event_listener_thread()
+
+exit()
---
+++ mouseInterfaces.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+
+import logidevmon
+import subprocess
+import time
+import json
+import sys
+import threading
+import asyncio
+import queue
+from kivy.logger import Logger
+
+
+mouse_event_listener_thread_handle = None
+server_process = None
+command_queue = queue.Queue()
+
+
+def processEvents(message):
+    #print(f"{message}")
+    message = json.loads(message)
+    if message["path"] == "divertedButtons" and message["value"]["cid1"] == 195:
+        print("Gesture button pushed")
+        command_queue.put("Gesture button")
+    if message["path"] == "divertedButtons" and message["value"]["cid1"] == 83:
+        command_queue.put("Program end")
+        return False
+    else:
+        return True
+
+
+
+def init_event_loop():
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
+def mouse_event_listener_thread():
+    global server_process
+    Logger.info("logidev: starting logi-devmon.exe")
+    server_process = subprocess.Popen(["logi-devmon.exe"], stdout=subprocess.DEVNULL)
+    sys.stdout.flush()
+    time.sleep(1)
+    Logger.info("logidev: logi-devmon.exe started")
+    sys.stdout.flush()
+
+    init_event_loop()
+    Logger.info("logidev: Event loop initiated")
+
+    mouseUnitId = 0
+    keyboardUnitId = 0
+
+    logidevmon.list_devices()
+    Logger.info("logidev: Devices listed")
+    logtech_devices = logidevmon.LOGITECH_DEVICES
+    if not isinstance(logtech_devices, list):
+        logtech_devices = [logtech_devices]
+    Logger.info(f"logidev: Devices: {logtech_devices}")
+
+    for device in logtech_devices:
+        if (device["type"] == "keyboard"):
+            keyboardUnitId = device['unitId']
+
+        if (device["type"] == "mouse"):
+            mouseUnitId = device['unitId']
+
+    if mouseUnitId == 0:
+        Logger.error("logidev: No mouse device found.")
+        exit()
+
+    Logger.info("logidev: Get Device info")
+    devinfo = logidevmon.get_device_info(mouseUnitId)
+    Logger.info(f"logidev: {devinfo}")
+    sys.stdout.flush()
+
+    if not devinfo["isConnected"]:
+        Logger.error("logidev: Mouse is not connected.")
+        exit()
+
+    logidevmon.set_specialKey_config(mouseUnitId, 86, True)
+    logidevmon.set_specialKey_config(mouseUnitId, 83, True)
+    logidevmon.set_specialKey_config(mouseUnitId, 195, True)
+    logidevmon.set_wheel_config(mouseUnitId, divert=True, hires=True, invert=False)
+    #logidevmon.set_spyConfig(mouseUnitId, spyButtons=True, spyKeys=False, spyPointer=False, spyThumbWheel=False, spyWheel=True)
+    logidevmon.read_events(processEvents)
+    Logger.info(f"logidev: Mouse Event Listener Thread Ended")
+
+def start_mouse_event_listener_thread():
+    global mouse_event_listener_thread_handle
+    mouse_event_listener_thread_handle = threading.Thread(target=mouse_event_listener_thread, name="MouseListenerThread", daemon=True)
+    mouse_event_listener_thread_handle.start()
+
+def stop_mouse_event_listener_thread():
+    global mouse_event_listener_thread_handle
+    if server_process is not None:
+        server_process.kill()
+        server_process.wait(2)
+        Logger.info(f"logidev: logi-devmon.exe exited with returncode {server_process.returncode}")
